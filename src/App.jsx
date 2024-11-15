57b4295cca8a6b556fd42378851d308ec3a2e3d4
Dmytro Loboda <dmytro.loboda@gmail.com>
2024-11-16 05:02:47 +0800

    movies list filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,58 @@
-/* eslint-disable max-len */
-import React from 'react';
+import { useState } from 'react';
+
 import './App.scss';
 import { MovieList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <MovieList movies={moviesFromServer} />
-    </div>
+export const App = () => {
+  const [query, setQuery] = useState('');
 
-    <div className="sidebar" data-cy="Sidebar">
-      Sidebar will be here
+  function prepareMovies(movies, queryLine) {
+    let preparedMovies = movies;
+
+    if (queryLine) {
+      const lowerCaseQuery = queryLine.trim().toLowerCase();
+
+      preparedMovies = preparedMovies.filter(
+        movie =>
+          movie.title.toLowerCase().includes(lowerCaseQuery) ||
+          movie.description.toLowerCase().includes(lowerCaseQuery),
+      );
+    }
+
+    return preparedMovies;
+  }
+
+  const visibleMovies = prepareMovies(moviesFromServer, query);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={e => {
+                  setQuery(e.currentTarget.value);
+                }}
+              />
+            </div>
+          </div>
+        </div>
+
+        <MovieList movies={visibleMovies} />
+      </div>
+
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-  </div>
-);
+  );
+};
